3d2006540ad15869ad2a4ad9fdde74fc9a069feb
augustjd <augustinspring@gmail.com>
2013-09-29 09:40:29 -0400

    Fixed the edit functionality

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,83 +3,98 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('AppCtrl', function ($scope, $http, $location) {
+controller('AppCtrl', function ($scope, $http, $location) {
 
     $scope.isActive = function(route) { 
-      return route === $location.path();
+        return route === $location.path();
     };
 
-  }).
-  controller('browseCtrl', function ($scope, $http, $rootScope, $location) {
-      $http({
+}).
+controller('browseCtrl', function ($scope, $http, $rootScope, $location) {
+    $http({
         method: 'GET',
         url: '/decks',
-      }).
-      success(function (data, status, headers, config) {
-        $rootScope.decks = data;
+    }).
+    success(function (data, status, headers, config) {
+        $rootScope.decks = data.result;
+
         console.log(data);
-      }).
-      error(function (data, status, headers, config) {
+    }).
+    error(function (data, status, headers, config) {
         console.log("error");
-      });
-  }).
-  controller('homeCtrl', function ($scope, $http, $rootScope, $location) {
-	  console.log("In home controller.");
+    });
+
+    $scope.vote = function(id) {
+        console.log("Voting...");
+        $http({
+            method: 'POST',
+            url: '/deck/vote/' + id
+        }).
+        success(function (data, status, headers, config) {
+            console.log("Voted");
+        }).
+        error(function (data, status, headers, config) {
+            console.log("Errord");
+        });
+    }
+}).
+controller('homeCtrl', function ($scope, $http, $rootScope, $location) {
+    console.log("In home controller.");
     $scope.submit = function() {
-      $scope.loading = true;
-      
-      $http({
-        method: 'GET',
-        url: '/decks/generate',
-        params: {q: $scope.formText,
-          adjs: 10, max_nouns: 30}
-      }).
-      success(function (data, status, headers, config) {
-        $rootScope.newDeck = data;
-        console.log(data);
-          $location.path("/create");
-      }).
-      error(function (data, status, headers, config) {
-        console.log("error");
-      });
+        $scope.loading = true;
+
+        $http({
+            method: 'GET',
+            url: '/decks/generate',
+            params: {q: $scope.formText,
+                adjs: 10, max_nouns: 30}
+        }).
+        success(function (data, status, headers, config) {
+            $rootScope.newDeck = data;
+            console.log(data);
+            $location.path("/create");
+        }).
+        error(function (data, status, headers, config) {
+            console.log("error");
+        });
 
     }
 
     $http({
         method: 'GET',
-        url: '/decks',
-        params: {sort_by: "vote"}
-      }).
-      success(function (data, status, headers, config) {
+    url: '/decks',
+    params: {sort_by: "vote"}
+    }).
+    success(function (data, status, headers, config) {
         $rootScope.voteDecks = data;
-      }).
-      error(function (data, status, headers, config) {
+    }).
+    error(function (data, status, headers, config) {
         console.log("error");
-      });
+    });
 
-      $http({
+    $http({
         method: 'GET',
         url: '/decks',
         params: {sort_by: "date"}
-      }).
-      success(function (data, status, headers, config) {
+    }).
+    success(function (data, status, headers, config) {
         $rootScope.dateDecks = data;
-      }).
-      error(function (data, status, headers, config) {
+    }).
+    error(function (data, status, headers, config) {
         console.log("error");
-      });
+    });
 
-      $http({
+    $http({
         method: 'GET',
         url: '/decks',
         params: {sort_by: "editorspick"}
-      }).
-      success(function (data, status, headers, config) {
+    }).
+    success(function (data, status, headers, config) {
         $rootScope.editorsDecks = data;
-      }).
-      error(function (data, status, headers, config) {
+    }).
+    error(function (data, status, headers, config) {
         console.log("error");
-      });
+    });
 
     $scope.recent = function() {
 
@@ -89,53 +104,51 @@ angular.module('myApp.controllers', []).
 
     }
 
-  }).
-  controller('createCtrl', function ($scope, $rootScope, $http, $location) {
-      $scope.publish = function(deck) {
-	      var real_deck = {
-		title: $scope.title,
-                description: $scope.description,
-                numOfCards: deck.nouns.length + deck.adjs.length,
-	        numOfNouns:deck.nouns.length,
-	  	numOfAdjs: deck.adjs.length,
-	  	numOfVotes:0,
-	  	tags: [],
-	  	nounCards:deck.nouns,
-	  	adjCards:deck.adjs,
-	  	generated:false,
-	  	editorsPick:false
-	      };
-	      console.log(real_deck.title);
-	      $http({
-		method: 'POST',
-		url: '/decks/create',
-		params: real_deck
-	      }).
-	      success(function (data, status, headers, config) {
-		$rootScope.newDeck = data;
-		  $location.path("/");
-	      }).
-	      error(function (data, status, headers, config) {
-		console.log("error");
-	      });
-      }
-  }).
-  controller('viewCtrl', function ($scope, $rootScope, $route, $routeParams, $http, $location) {
-      $http({
+}).
+controller('createCtrl', function ($scope, $rootScope, $http, $location) {
+    $scope.publish = function(deck) {
+        var real_deck = {
+            title: $scope.title,
+description: $scope.description,
+numOfCards: deck.nouns.length + deck.adjs.length,
+numOfNouns:deck.nouns.length,
+numOfAdjs: deck.adjs.length,
+numOfVotes:0,
+tags: [],
+nounCards:deck.nouns,
+adjCards:deck.adjs,
+generated:false,
+editorsPick:false
+        };
+        console.log(real_deck.title);
+        $http({
+            method: 'POST',
+            url: '/decks/create',
+            params: real_deck
+        }).
+        success(function (data, status, headers, config) {
+            $rootScope.newDeck = data;
+            $location.path("/");
+        }).
+        error(function (data, status, headers, config) {
+            console.log("error");
+        });
+    }
+}).
+controller('viewCtrl', function ($scope, $rootScope, $route, $routeParams, $http, $location) {
+    $http({
         method: 'GET',
-        url: '/deck/' + $routeParams.deck_id
-      }).
-      success(function (data, status, headers, config) {
+    url: '/deck/' + $routeParams.deck_id
+    }).
+    success(function (data, status, headers, config) {
         $rootScope.deck = data;
         console.log(data);
-      }).
-      error(function (data, status, headers, config) {
+    }).
+    error(function (data, status, headers, config) {
         console.log("error");
-	$location.path('/')
-      });
+        $location.path('/')
+    });
 
-  }).
-  controller('aboutCtrl', function ($scope) {
-    // write Ctrl here
-
-  });
+}).
+controller('aboutCtrl', function ($scope) {
+});
